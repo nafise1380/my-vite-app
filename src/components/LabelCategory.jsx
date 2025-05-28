b344aac375f508e0b1fb7f41a562c56bf590f1ed
nafas <nfis71800@gmail.com>
2025-05-28 13:16:43 +0330

    icon added

--- src/components/LabelCategory.jsx
+++ src/components/LabelCategory.jsx
@@ -1,17 +1,38 @@
 
-export default function LabelCategory(){
-    return(
-        <>
-             <label className="d-flex "> دسته بندی </label>
-                <select name='دسته بندی' className="bg-info w-100 p-2" >
-                     <option value="item1">  دسته بندی </option>
-                     <option value="item2"> آیتم دوم </option>
-                     <option value="item3"> آیتم سوم </option>
-                </select>
+// export default function LabelCategory(){
+//     return(
+//         <>
+//              <label className="d-flex  justify-content-end  mb-2 mx-3 lead "> دسته بندی </label>
+//              <img src="/icon.svg" alt="icon" className="position-absolute"/>
+//                 <select name='دسته بندی' className=" w-100 p-2 text-" >
+                    
+//                      <option value="item1">  دسته بندی </option>
+//                      <option value="item2"> آیتم دوم </option>
+//                      <option value="item3"> آیتم سوم </option>
+//                 </select>
         
         
         
         
-        </>
-    )
+//         </>
+//     )
+// }
+
+export default function LabelCategory() {
+  return (
+    <>
+      <div className="d-flex align-items-center justify-content-end mb-2 mx-3 lead">
+        <img src="/icon.svg" alt="icon" className="position-absolute mx-3 "  style={{height: '30px',  marginTop:'100px' }} />
+        <label className="m-0">دسته بندی</label>
+      </div>
+
+      <div className=" p-2">
+        <select name="دسته بندی" className="w-100 p-2">
+          <option value="item1">دسته بندی</option>
+          <option value="item2">آیتم دوم</option>
+          <option value="item3">آیتم سوم</option>
+        </select>
+      </div>
+    </>
+  );
 }
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -5,11 +5,11 @@ export default function Section(){
         <>
         
           <section className="border-bottom">
-            <span> سایر</span>
-            <div className="d-flex justify-content-between">
+            <span className=" d-flex justify-content-end my-2  mb-3"> سایر</span>
+            <div className="d-flex ">
               <a href='#'> <img src='/Alt Arrow Left.svg'></img></a>
-              <span> کل ایران</span>
-              <p className="bg-info "> استان</p>
+              <span className=""> کل ایران</span>
+              <p className="bg-info  "> استان</p>
             </div>
           </section>
         
