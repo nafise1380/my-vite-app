4f5cce27932679304bd0f87ccf90993ace2cb8d6
nafas <nfis71800@gmail.com>
2025-07-03 15:44:58 +0330

    spaces added

--- src/components/InputFourChoice.jsx
+++ src/components/InputFourChoice.jsx
@@ -5,8 +5,8 @@ function InputFourChoice() {
   return (
          <>
             <div className="container border-b border-gray-200">
-                <span className="flex justify-end font-bold"> نوع وکیل </span>
-                <div className="flex justify-end justify-between">
+                <span className="flex justify-end font-bold mx-3 mt-3"> نوع وکیل </span>
+                <div className="flex justify-end justify-between mx-2">
                    <div className=" my-3 mx-12">
                         <label  className="mx-2 font-bold"> همه </label>
                         <input type="radio" name="sender"/>
