29eefe251e7c522cf3be80ea9f9fabca66c2ec02
nafas <nfis71800@gmail.com>
2025-07-15 22:36:46 +0330

    event and props added

--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -9,7 +9,8 @@ export default function ProfilePage(){
           <Headers/>
           <Navbar/>
           <ProfileSection/>
-          <SectionOfConsultation/>
+          <SectionOfConsultation subject="تعرفه مشاوره" imageSrc="/phone.svg" text="  مشاوره تلفنی فوری" price="۲۰ دقیقه ۱۰۰ هزار تومان" button=" تماس فوری"/> 
+          <SectionOfConsultation list1="برای تنظیم قرارداد" list2="تنظیم شکایت" imageSrc="icon-wrapper.svg" text="مشاوره متنی فوری" price="۲۰ دقیقه ۹۰ هزار تومان" button=" چت فوری"/>
         </>
     )
 }
--- src/components/Profile/SectionOfTelephoneConsultation.jsx
+++ src/components/Profile/SectionOfTelephoneConsultation.jsx
@@ -1,19 +1,28 @@
 
 
-function SectionOfTelephoneConsultation() {
+function SectionOfTelephoneConsultation(props) {
+  function clickHandler(){
+    console.log("clickkkk")
+  }
   return (
      < >
-        <span className="container flex "> تعرفه مشاوره </span>
+        <span className="container flex ">  {props.subject} </span>
         <section className="container border p-3 gap-4 "  style={{borderRadius:"7px"}}>
                <div className="flex border-b border-dashed border-green-300">
-                     <img src="/phone-calling-rounded.svg " alt="phone-calling-rounded"/>
-                     <p className="font-bold text-lg "> مشاوره تلفنی فوری </p>
+                     {props.imageSrc}
+                     <p className="font-bold text-lg "> {props.text} </p>
                </div>
+               {(props.list1 || props.list2) && (
+                  <ul className="list-decimal pl-5">
+                   {props.list1 && <li>{props.list1}</li>}
+                   {props.list2 && <li>{props.list2}</li>}
+                 </ul>
+               )}
                 <div className="mt-3">
-                    <span> ۲۰ دقیقه ۱۰۰ هزار تومان </span>
+                    <span> {props.price}   </span>
                     <div className=" flex bg-green-500 justify-center ">
                         <img src="/phone-calling-rounded.svg" alt="phone-calling-rounded"/>
-                        <button className="m-2 " onClick={{}}>  تماس فوری</button>
+                        <button className="m-2 " onClick={clickHandler}>  {props.button} </button>
                     </div>
                 </div>
         </section>
